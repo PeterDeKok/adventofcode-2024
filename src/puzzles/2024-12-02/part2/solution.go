faf712c8e01b98bb7e4fda5b07d9a16408ee76a6
Peter de Kok <info@peterdekok.nl>
2024-12-02 19:36:59 +0100

    solution(d02-p02): Red-Nosed Reports

---
+++ src/puzzles/2024-12-02/part2/solution.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/build"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/tools/input"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/tools/logger"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/tools/math"
+	"strconv"
+)
+
+func Solution(_ context.Context, _ *logger.IterationLogger, rd io.Reader, w io.Writer) error {
+	var safe int
+
+	for i, line := range input.LineIsIntSliceReader(rd) {
+		lineResult, ok, err := handleLine(line)
+		if !ok && err != nil {
+			panic(fmt.Errorf("failed to handle line %d: %v", i, err))
+		} else if !ok {
+			panic(fmt.Errorf("failed to handle line %d: unknown error", i))
+		}
+
+		if build.DEBUG {
+			if err != nil {
+				fmt.Printf("[line %d | %v]: %v | %v\n", i, line, lineResult, err)
+			} else {
+				fmt.Printf("[line %d | %v]\n", i, line)
+			}
+		}
+
+		if lineResult {
+			if build.DEBUG {
+				fmt.Printf("[line %d]: %v\n", i, lineResult)
+			}
+			safe++
+			continue
+		}
+
+		for j := 0; j < len(line); j++ {
+			filtered := make([]int, len(line)-1)
+			copy(filtered[:j], line[:j])
+			copy(filtered[j:], line[j+1:])
+
+			lineResult, ok, err = handleLine(filtered)
+			if !ok && err != nil {
+				panic(fmt.Errorf("failed to handle line %d: %v", i, err))
+			} else if !ok {
+				panic(fmt.Errorf("failed to handle line %d: unknown error", i))
+			}
+
+			if build.DEBUG {
+				if err != nil {
+					fmt.Printf("  [line %d | f: %d | %v]: %v | %v\n", i, j, filtered, lineResult, err)
+				} else {
+					fmt.Printf("  [line %d | f: %d | %v]: %v\n", i, j, filtered, lineResult)
+				}
+			}
+
+			if lineResult {
+				safe++
+				break
+			}
+		}
+
+		if build.DEBUG {
+			fmt.Printf("[line %d]: %v\n", i, lineResult)
+		}
+	}
+
+	if _, err := w.Write([]byte(strconv.Itoa(safe))); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handleLine(line []int) (bool, bool, error) {
+	if len(line) < 2 {
+		return false, false, fmt.Errorf("invalid report, len = %d", len(line))
+	}
+
+	var dirUp, dirDown, dirZero int
+
+	for i, v := range line[1:] {
+		if v > line[i] {
+			dirUp++
+		} else if v < line[i] {
+			dirDown++
+		} else {
+			dirZero++
+		}
+	}
+
+	if dirZero > 0 {
+		return false, true, fmt.Errorf("zero diff")
+	} else if dirUp > 0 && dirDown > 0 {
+		return false, true, fmt.Errorf("not strictly increasing or decreasing")
+	}
+
+	for i, v := range line[1:] {
+		if diff := math.AbsDiff(v, line[i]); diff > 3 {
+			return false, true, fmt.Errorf("diff too large d[%d, %d] = %d", v, line[i], diff)
+		}
+	}
+
+	return true, true, nil
+}
