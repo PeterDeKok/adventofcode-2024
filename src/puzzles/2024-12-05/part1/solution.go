a9f0f21b1d180f3d254d21ab7d41663e7e73cd8a
Peter de Kok <info@peterdekok.nl>
2024-12-05 09:33:03 +0100

    solution(d05-p01): Print Queue

---
+++ src/puzzles/2024-12-05/part1/solution.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/build"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/tools/input"
+	"peterdekok.nl/adventofcode/twentytwentyfour/src/tools/logger"
+	"strconv"
+	"strings"
+)
+
+func Solution(_ context.Context, _ *logger.IterationLogger, rd io.Reader, w io.Writer) error {
+	var sum int
+	var ordersI int
+
+	lookup := make(map[int][]int)
+	stage := "lookup"
+
+	for i, line := range input.LineReader(rd) {
+		if line == "" {
+			if build.DEBUG {
+				fmt.Printf("\n\n%v\n\n", lookup)
+			}
+			stage = "orders"
+			continue
+		}
+
+		if build.DEBUG {
+			fmt.Printf("[parse: %d | %s]: %s\n", i, stage, line)
+		}
+
+		switch stage {
+		case "lookup":
+			v := strings.Split(line, "|")
+			if len(v) != 2 {
+				panic("Invalid constriant")
+			}
+
+			key, err := strconv.Atoi(v[0])
+			if err != nil {
+				panic("Failed to parse key")
+			}
+
+			value, err := strconv.Atoi(v[1])
+			if err != nil {
+				panic("Failed to parse key")
+			}
+
+			constraints, ok := lookup[key]
+			if !ok {
+				constraints = make([]int, 0, 10)
+				lookup[key] = constraints
+			}
+
+			lookup[key] = append(lookup[key], value)
+			if build.DEBUG {
+				fmt.Printf("  [rule: %d]: %s\n", i, line)
+			}
+		default:
+			nr, ok := handlePrintLine(lookup, line)
+
+			sum += nr
+
+			if build.DEBUG {
+				fmt.Printf("  [order: %d]: %v %s\n", ordersI, ok, line)
+			}
+
+			ordersI++
+		}
+	}
+
+	if _, err := w.Write([]byte(strconv.Itoa(sum))); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handlePrintLine(lookup map[int][]int, line string) (int, bool) {
+	strs := strings.Split(line, ",")
+	nrs := make([]int, len(strs))
+	nrsLookup := make(map[int]int, len(strs))
+
+	for i, v := range strs {
+		nr, err := strconv.Atoi(v)
+		if err != nil {
+			panic(fmt.Errorf("failed to parse value %d", i))
+		}
+		nrs[i] = nr
+
+		constraints, ok := lookup[nr]
+		if !ok {
+			if build.DEBUG {
+				fmt.Printf("    [check: %d] no constraint\n", nr)
+			}
+			nrsLookup[nr] = nr
+			continue
+		}
+
+		if build.DEBUG {
+			fmt.Printf("    [check: %d]: against %v\n", nr, constraints)
+		}
+
+		for _, before := range constraints {
+			if _, wrong := nrsLookup[before]; wrong {
+				if build.DEBUG {
+					fmt.Printf("    [check: %d]: not before %d\n", nr, before)
+				}
+				return 0, false
+			} else {
+				if build.DEBUG {
+					fmt.Printf("    [check: %d]: checked against %d\n", nr, before)
+				}
+			}
+		}
+
+		nrsLookup[nr] = nr
+	}
+
+	mid := nrs[len(nrs)/2]
+
+	if build.DEBUG {
+		fmt.Printf("    [check]: OK: mid: %d\n", mid)
+	}
+	return mid, true
+}
